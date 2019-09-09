8efcf79d23fa75ba87b6e66a3c6b8de455b53977
github username <github email address>
2019-09-09 14:45:16 -0400

    more backend work

--- eighties-emotions-backend/config/routes.rb
+++ eighties-emotions-backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :artists
+  resources :genres, only: [:index, :show]
   resources :years, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- eighties-emotions-backend/db/schema.rb
+++ eighties-emotions-backend/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_133756) do
+ActiveRecord::Schema.define(version: 2019_09_09_183929) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "years", force: :cascade do |t|
     t.integer "name"
