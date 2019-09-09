ef46c876334773fcc39b6bdf40585f029a1bea54
github username <github email address>
2019-09-09 14:48:14 -0400

    more backend work

--- eighties-emotions-backend/config/routes.rb
+++ eighties-emotions-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: [:index, :show]
   resources :genres, only: [:index, :show]
   resources :years, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
